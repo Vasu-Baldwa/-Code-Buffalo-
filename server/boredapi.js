7011478e32f504093bdf3c4de6283323f8dd2bf5
MostlyFireproof <iranoutofemailadressideas@gmail.com>
2019-06-15 13:24:17 -0400

    more of my code

---
+++ server/boredapi.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const bored {
+  activity: String
+  accessibility: Float
+  type: String
+  participants: Int,
+  price: Float,
+  link: String,
+  key: Int
+}
