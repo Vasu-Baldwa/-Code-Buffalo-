a4150d5e73e1f8ec8ea190aa5296f41f8c8b1359
Vasu Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2019-06-15 11:02:47 -0400

    Removed User

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 const schema = require(".//schema//schema");
 const app = express();
 
-mongoose.connect("mongodb+srv://me1:<password>@cluster0-qgqp7.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://me1:<Password>@cluster0-qgqp7.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
 })
--- server/user.js
+++ server/user.js
@@ -4,14 +4,11 @@ const Schema = mongoose.Schema;
 
 const User = new GraphQLObjectType({
     name: "User",
-    fields: () => ({
         userID:{type: GraphQLID},
         psswd:{type: GraphQLString},
         avgPrice:{type: GraphQLFloat},
         priceN:{type: GraphQLFloat}
         //typesN
-    })
-
 })
 
-module.exports = mongoose.model('Author', authorSchema);
+module.exports = mongoose.model('User', userSchema);
